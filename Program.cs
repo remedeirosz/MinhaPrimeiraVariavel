ded4ff6b40955aaa429478c1842b9c204c704999
Renata Medeiros <camilly19medeiros@gmail.com>
2026-09-16 16:56:55 -0300

    cria variavel para usar depois

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// este programa deverá salvar o nome do usuário dentro de uma variavel 
+//este programa deverá exibir uma mensagem de saudação mostrando o nome do usuário
+var nomeUsuario = "Medeiros";
+Console.Write("Boas vindas, ");
+Console.WriteLine(nomeUsuario);
